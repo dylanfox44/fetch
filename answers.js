2199e8b79804e9ec5a4c131ddc0bcb3a6592e8f9
dylanfox44 <131827295+dylanfox44@users.noreply.github.com>
2025-02-17 03:16:17 +0000

    answers

--- answers.js
+++ answers.js
@@ -1,1 +1,12 @@
-// Add JavaScript below
+let distance = 384400, diameter = 3474;
+
+console.log(distance / diameter); // how many moons between the moon and Earth
+
+let x = 52089, y = 51316;
+
+console.log(((x - y) / y) * 100); // enrollment change
+
+let nintendo = 8, supernin = 16; // nintendo bits
+
+console.log(nintendo * supernin);
+
